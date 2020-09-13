c4399a8549ef01d242b4e74886afbc7083d30321
carignya <anthonycarigny@rentalcars.com>
2020-09-13 22:33:12 +0100

    Updates

---
+++ precache-manifest.5768dedadd5f412cb65d090e84ccecd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d44765aa0ef6b0345c1251236084b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21440d884872132b78ba",
+    "url": "/static/css/main.f1cbdc9e.chunk.css"
+  },
+  {
+    "revision": "d73a41613a33c4c5a5af",
+    "url": "/static/js/2.de40b800.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de40b800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21440d884872132b78ba",
+    "url": "/static/js/main.d9fa6401.chunk.js"
+  },
+  {
+    "revision": "26b538121099287fc027",
+    "url": "/static/js/runtime-main.dfa6e668.js"
+  }
+]);
